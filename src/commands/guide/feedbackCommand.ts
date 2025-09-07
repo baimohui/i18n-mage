b2484530d03ea80037f98ca9b5ee160d47ba806d
Jensen <mohui1020@gmail.com>
2025-09-07 16:57:12 +0800

    feat: auto open i18nMage view after usage check

--- src/commands/guide/feedbackCommand.ts
+++ src/commands/guide/feedbackCommand.ts
@@ -3,7 +3,7 @@ import { registerDisposable } from "@/utils/dispose";
 
 export function registerFeedbackCommand() {
   const disposable = vscode.commands.registerCommand("i18nMage.feedback", () => {
-    vscode.env.openExternal(vscode.Uri.parse("https://github.com/baimohui/i18n-mage/issues"));
+    vscode.env.openExternal(vscode.Uri.parse("https://github.com/baimohui/i18n-mage/issues/new"));
   });
 
   registerDisposable(disposable);
